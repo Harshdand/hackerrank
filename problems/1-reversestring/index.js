4bc3f9007ff2ced2b93c107de927c9e6ee09b97e
harsh <harshdand2@gmail.com>
2018-05-04 00:26:13 +0530

    added string reverse and palindrome

---
+++ problems/1-reversestring/index.js
@@ -0,0 +1,45 @@
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'leppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
+
+function reverse(str) {
+    return str.split('')
+        .reduce((rev, char) => {
+            return char + rev;
+        }, ''); // initial value
+}
+
+// function reverse(str) {
+//     return str.split('').reduce((rev, char) => char + rev, '');
+// }
+
+// function reverse(str) {
+//     let reversed = '';
+
+//     for (let character of str) {
+//         reversed = character + reversed;
+//     }
+
+//     return reversed;
+// }
+
+// function reverse(str) {
+//     const strLength = str.length;
+//     let reversed = '';
+
+//     for (let i = strLength - 1; i >= 0; i--) {
+//         reversed += str[i];
+//     }
+
+//     return reversed;
+// }
+
+// function reverse(str) {
+//     return str.split('').reverse().join('');
+// }
+
+module.exports = reverse;
