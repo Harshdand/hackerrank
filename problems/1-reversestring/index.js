786a76ca799e1082cefdfec90c0ed4f913ac8b8e
harsh <harshdand2@gmail.com>
2018-05-04 00:32:18 +0530

    fixed comments

--- problems/1-reversestring/index.js
+++ problems/1-reversestring/index.js
@@ -1,10 +1,11 @@
-// --- Directions
-// Given a string, return a new string with the reversed
-// order of characters
-// --- Examples
-//   reverse('apple') === 'leppa'
-//   reverse('hello') === 'olleh'
-//   reverse('Greetings!') === '!sgniteerG'
+/* --- Directions
+Given a string, return a new string with the reversed
+order of characters
+--- Examples
+  reverse('apple') === 'leppa'
+  reverse('hello') === 'olleh'
+  reverse('Greetings!') === '!sgniteerG'
+*/
 
 function reverse(str) {
     return str.split('')
@@ -13,33 +14,41 @@ function reverse(str) {
         }, ''); // initial value
 }
 
-// function reverse(str) {
-//     return str.split('').reduce((rev, char) => char + rev, '');
-// }
+/*
+function reverse(str) {
+    return str.split('').reduce((rev, char) => char + rev, '');
+}
+*/
 
-// function reverse(str) {
-//     let reversed = '';
+/*
+function reverse(str) {
+    let reversed = '';
 
-//     for (let character of str) {
-//         reversed = character + reversed;
-//     }
+    for (let character of str) {
+        reversed = character + reversed;
+    }
 
-//     return reversed;
-// }
+    return reversed;
+}
+*/
 
-// function reverse(str) {
-//     const strLength = str.length;
-//     let reversed = '';
+/*
+function reverse(str) {
+    const strLength = str.length;
+    let reversed = '';
 
-//     for (let i = strLength - 1; i >= 0; i--) {
-//         reversed += str[i];
-//     }
+    for (let i = strLength - 1; i >= 0; i--) {
+        reversed += str[i];
+    }
 
-//     return reversed;
-// }
+    return reversed;
+}
+*/
 
-// function reverse(str) {
-//     return str.split('').reverse().join('');
-// }
+/*
+function reverse(str) {
+    return str.split('').reverse().join('');
+}
+*/
 
 module.exports = reverse;
--- problems/2-palindrome/index.js
+++ problems/2-palindrome/index.js
@@ -1,11 +1,12 @@
-// --- Directions
-// Given a string, return true if the string is a palindrome
-// or false if it is not.  Palindromes are strings that
-// form the same word if it is reversed. *Do* include spaces
-// and punctuation in determining if the string is a palindrome.
-// --- Examples:
-//   palindrome("abba") === true
-//   palindrome("abcdefg") === false
+/* --- Directions
+Given a string, return true if the string is a palindrome
+or false if it is not.  Palindromes are strings that
+form the same word if it is reversed. *Do* include spaces
+and punctuation in determining if the string is a palindrome.
+--- Examples:
+  palindrome("abba") === true
+  palindrome("abcdefg") === false
+*/
 
 function palindrome(str) {
     return str.split('').every((char, index) => {
@@ -13,29 +14,32 @@ function palindrome(str) {
     });
 }
 
-// function palindrome(str) {
-//     var a = 0;
-//     var res = false;
+/*
+function palindrome(str) {
+    var a = 0;
+    var res = false;
 
-//     str.split('').every((char, index) => {
-//         a++;
+    str.split('').every((char, index) => {
+        a++;
 
-//         if (str.length / 2 === index) {
-//             return false;
-//         }
+        if (str.length / 2 === index) {
+            return false;
+        }
 
-//         console.log(a);
-//         res = char === str[str.length - index - 1];
+        console.log(a);
+        res = char === str[str.length - index - 1];
 
-//         return res;
-//     });
+        return res;
+    });
 
-//     return res;
-// }
+    return res;
+}
+*/
 
-
-// function palindrome(str) {
-//     return str === str.split('').reverse().join('');
-// }
+/*
+function palindrome(str) {
+    return str === str.split('').reverse().join('');
+}
+*/
 
 module.exports = palindrome;
