40c97650299bb86df3a801e01cdce7c3cbb07a16
harsh <harshdand2@gmail.com>
2018-05-08 00:16:15 +0530

    stack using object

---
+++ problems/20-stack-simple-object/index.js
@@ -0,0 +1,40 @@
+// --- Directions
+// Create a stack data structure.  The stack
+// should be a class with methods 'push', 'pop', and
+// 'peek'.  Adding an element to the stack should
+// store it until it is removed.
+// --- Examples
+//   const s = new Stack();
+//   s.push(1);
+//   s.push(2);
+//   s.pop(); // returns 2
+//   s.pop(); // returns 1
+
+// stack based on indexes, does'nt remove the items
+class Stack {
+    constructor() {
+        this.data = {};
+        this.top = -1;
+    }
+
+    push(record) {
+        this.data[++this.top] = record;
+    }
+
+    pop() {
+        const record = this.data[this.top];
+        delete this.data[this.top--];
+
+        return record;
+    }
+
+    peek() {
+        return this.data[this.top];
+    }
+
+    isEmpty() {
+        return this.top === -1;
+    }
+}
+
+module.exports = Stack;
